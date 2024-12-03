06b6740627e1de400bc23e323ec6394e2df27ab4
Tane4ka170 <tane4ka170@gmail.com>
2024-12-03 20:03:33 +0200

    Added idle & jump animations

--- vanilla-js-spritely/script.js
+++ vanilla-js-spritely/script.js
@@ -9,16 +9,23 @@ playerImage.src = "shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
-let frameY = 4;
+let frameY = 5;
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  { name: "idle", frame: 7 },
+  { name: "jump", frame: 7 },
+];
+animationStates.forEach((state, index) => {});
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame / staggerFrames) % 6;
+  let position = Math.floor(gameFrame / staggerFrames) % 11;
+  frameX = spriteWidth * position;
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth,
+    frameX,
     frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
